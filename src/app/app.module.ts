af381d96f5cfc361bf3faf001c7f3baeff2a2ffb
memopresso <memopresso@memopressode-MacBook-Pro.local>
2018-05-16 18:28:48 +0800

    set up home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TypeComponent } from './type/type.component';
+import { SelectComponent } from './select/select.component';
+import { PreviewComponent } from './preview/preview.component';
+
+
+const appRoutes: Routes = [
+  {path: 'preview', component: PreviewComponent},
+  {path: 'select', component: SelectComponent},
+  {path: 'type', component: TypeComponent},
+  {path: '', component: HomeComponent},
+  {path: '**', component: HomeComponent},
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TypeComponent,
+    SelectComponent,
+    PreviewComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/type/type.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-type',
+  templateUrl: './type.component.html',
+  styleUrls: ['./type.component.scss']
+})
+export class TypeComponent implements OnInit, OnDestroy {
+
+  sub: Subscription;
+  entry: string;
+
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+    this.sub = this.route.queryParams.subscribe(params => {
+      this.entry = params['entry'];
+      this.checkEntry();
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  checkEntry() {
+    // if no entry then return home
+    if (!this.entry) {
+      this.router.navigate(['']);
+    }
+
+    // if wrong entry then set default
+    switch (this.entry) {
+      case 'tag':
+        break;
+      case 'ig':
+        break;
+      default:
+        this.entry = 'tag';
+    }
+  }
+
+
+
+}
